e8b3d2a7033930105d71da81b53ad8810ed0b462
liuyuan <709503583@qq.com>
2021-11-23 16:49:33 +0800

    Removes the provider setting and improves the Readme

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/maxcompute"
-}
-
 resource "alicloud_maxcompute_project" "this" {
   name               = var.name
   specification_type = var.specification_type
