b71f994e66940b4c019446c6e9fd716c28003b28
shanye <shanye.xzq@alibaba-inc.com>
2024-03-25 19:48:47 +0800

    resource/alicloud_maxcompute_project: support attributes project_name, product_type, comment, default_quota; improve the examples

--- variables.tf
+++ variables.tf
@@ -29,13 +29,40 @@ variable "name" {
 }
 
 variable "specification_type" {
-  description = "The type of resource Specification, only `OdpsStandard` supported currently."
+  description = "(Deprecated from version 1.2.0) The type of resource Specification, only `OdpsStandard` supported currently."
   type        = string
   default     = "OdpsStandard"
 }
 
 variable "order_type" {
-  description = "The type of payment, only `PayAsYouGo` supported currently."
+  description = "(Deprecated from version 1.2.0) The type of payment, only `PayAsYouGo` supported currently."
   type        = string
   default     = "PayAsYouGo"
+}
+
+variable "product_type" {
+  description = "Quota payment type. Supported values: `PayAsYouGo`, `Subscription` and `Dev`."
+  type        = string
+  default     = null
+}
+
+variable "comment" {
+  description = "Comments of project"
+  type        = string
+  default     = null
+}
+
+variable "default_quota" {
+  description = "Default Computing Resource Group"
+  type        = string
+  default     = null
+}
+
+variable "ip_white_list" {
+  description = "IP whitelist. ip_list: Classic network IP white list. vpc_ip_list: VPC network whitelist."
+  type = list(object({
+    ip_list     = string
+    vpc_ip_list = string
+  }))
+  default = []
 }
