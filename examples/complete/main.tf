f8c31e41508366bb7e3a906cf2c60b054056a19b
shanye <shanye.xzq@alibaba-inc.com>
2024-11-25 16:44:43 +0800

    examples: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,21 +1,5 @@
-variable "profile" {
-  default = "default"
-}
-variable "region" {
-  default = "cn-hangzhou"
-}
-
-
-provider "alicloud" {
-  region  = var.region
-  profile = var.profile
-}
-
-
 module "maxcompute_project" {
-  source  = "../.."
-  region  = var.region
-  profile = var.profile
+  source = "../.."
 
   #project
   name          = "my_mc_project"
